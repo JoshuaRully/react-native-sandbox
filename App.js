2294a783beeb7670d36b5b71531f65b5435d3656
Joshua Rully <joshrully@gmail.com>
2021-08-13 13:14:39 -0400

    setup simple button counter

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>You clicked {count} times!</Text>
+      <Button
+        onPress={() => setCount(count + 1)}
+        title="Click me!"
+      />
     </View>
   );
 }
